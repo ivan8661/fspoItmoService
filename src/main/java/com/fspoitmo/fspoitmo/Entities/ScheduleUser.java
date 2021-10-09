c1d22e9294f3a6245d848924d79b0d5cbd6e553d
Kirill Kovalev <kirilkovalev@yandex.ru>
2021-10-09 22:21:28 +0300

    * | ScheduleUser | dded annotations for correct json serialization

--- src/main/java/com/fspoitmo/fspoitmo/Entities/ScheduleUser.java
+++ src/main/java/com/fspoitmo/fspoitmo/Entities/ScheduleUser.java
@@ -1,12 +1,15 @@
 package com.fspoitmo.fspoitmo.Entities;
 
 import GetGraphQL.SearchableField;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ScheduleUser {
 
+    @JsonProperty("_id")
     private String id;
 
     @SearchableField
+    @JsonProperty("name")
     private String name;
 
     public ScheduleUser(String id, String name) {
